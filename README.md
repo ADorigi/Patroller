c1195b737415e2c4fb45e9529130b02ebadded2e
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-10-24 23:10:02 -0400

    changes to add vpc

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Patroller
-Cloud Provisioning as a Service
+
+Terraform GCP configuration files
+
+Identity pool and service account creted in adorigi/baobab
+
+
+<!-- - create a budget in gcp
+- create a bucket storage in gcp
+- make that bucket a backend to store the state files for future deployments -->
---
+++ terraform.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0" # OIDC https://github.com/hashicorp/terraform-provider-google/releases/tag/v3.61.0
+    }
+  }
+
+  backend "gcs" {
+    bucket = "agile-tangent-terraform" # need to update with the bucket name
+    prefix = "state"
+  }
+}
