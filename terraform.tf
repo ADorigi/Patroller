5118f6ae1204e5a5ac49e62a26654a99b29faeee
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-10-25 12:07:25 -0400

    using workspaces

--- terraform.tf
+++ terraform.tf
@@ -8,6 +8,6 @@ terraform {
 
   backend "gcs" {
     bucket = "agile-tangent-terraform" # need to update with the bucket name
-    prefix = var.prefix
+    prefix = "state/"
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,3 @@
-variable "prefix" {
-  type    = string
-}
+# variable "prefix" {
+#   type    = string
+# }
