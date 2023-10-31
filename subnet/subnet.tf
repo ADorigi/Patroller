001a0fc8137bd97853be424a1886eb2d728d1469
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-10-31 17:15:17 -0400

    run next step on fmt failure subnet name correction

--- subnet/subnet.tf
+++ subnet/subnet.tf
@@ -1,5 +1,5 @@
 resource "google_compute_subnetwork" "public-subnet" {
-  name          = "public_subnet"
+  name          = "public-subnet"
   ip_cidr_range = "10.2.0.0/16"
   region        = "northamerica-northeast2"
   network       = local.vpc_name
