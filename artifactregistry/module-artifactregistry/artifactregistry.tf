976c2ad64ad73333b0cc95cd659f12599ef91def
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-11-08 16:32:02 -0500

    artifact registry

---
+++ artifactregistry/module-artifactregistry/artifactregistry.tf
@@ -0,0 +1,6 @@
+resource "google_artifact_registry_repository" "artifact-registry" {
+  location      = var.region 
+  repository_id = var.repository-id 
+  description   = var.description 
+  format        = var.format
+}
---
+++ artifactregistry/module-artifactregistry/variables.tf
@@ -0,0 +1,16 @@
+variable "repository-id" {
+    type = string
+    default = "default-repo-id"
+}
+variable "region" {
+  type    = string
+  default = "northamerica-northeast2"
+}
+variable "description" {
+    type = string
+    default = "default description"
+}
+variable "format" {
+    type = string
+    default = "DOCKER"
+}
