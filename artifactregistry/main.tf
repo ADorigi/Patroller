48fb0325e388d606fcf8733c10a1812382a0f69f
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-11-08 17:01:47 -0500

    formatted terraform

--- artifactregistry/main.tf
+++ artifactregistry/main.tf
@@ -1,6 +1,6 @@
 module "artifact-registry" {
-  source = "./module-artifactregistry"
-  region = "northamerica-northeast2"
+  source      = "./module-artifactregistry"
+  region      = "northamerica-northeast2"
   description = "artifact registry for swath"
-  format = "DOCKER"
+  format      = "DOCKER"
 }
