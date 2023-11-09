bc6f622b6c01e2e64a67aaacae19f7067c79bb3a
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-11-08 20:57:33 -0500

    resources id added

--- artifactregistry/main.tf
+++ artifactregistry/main.tf
@@ -1,6 +1,7 @@
 module "artifact-registry" {
-  source      = "./module-artifactregistry"
-  region      = "northamerica-northeast2"
-  description = "artifact registry for swath"
-  format      = "DOCKER"
+  source        = "./module-artifactregistry"
+  repository-id = "container-registry"
+  region        = "northamerica-northeast2"
+  description   = "artifact registry for swath"
+  format        = "DOCKER"
 }
