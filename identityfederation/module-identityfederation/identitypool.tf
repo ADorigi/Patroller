a49a08b8ff2456906528c40ec30f0680fc0988dc
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-11-09 13:03:32 -0500

    updated name for pool

--- identityfederation/module-identityfederation/identitypool.tf
+++ identityfederation/module-identityfederation/identitypool.tf
@@ -1,4 +1,4 @@
-resource "google_iam_workload_identity_pool" "identity-pool" {
+resource "google_iam_workload_identity_pool" "workload_identity_pool" {
   workload_identity_pool_id = var.workload_identity_pool_id
   display_name              = var.idenity_pool_name
   description               = var.idenity_pool_description
