c3807c9491f957d845cd242a80dd6e60c971d858
Adnan Gulegulzar <gulegulzaradnan@gmail.com>
2023-12-21 15:10:13 -0500

    faith only branch

--- faith.tf
+++ faith.tf
@@ -1,5 +1,6 @@
 
 locals {
+  vpc_name = "patroller-vpc"
   faith_region = "us-east1"
   useast1_cidr = "11.1.0.0/16" 
 }
@@ -9,8 +10,8 @@ module "subnet" {
   source                   = "./module-subnet"
   subnet_name              = "subnet-${local.faith_region}"
   ip_cidr_range            = local.useast1_cidr
-  region                   = local.region
-  vpc_name                 = module.google-network.name
+  region                   = local.faith_region
+  vpc_name                 = local.vpc_name
   private_ip_access        = "false"
   secondary_ip_range_names = []
   secondary_ip_ranges      = []
